56dd07be5c9a2e09af057cbebc699bd08ecd6205
Sana Moussa <sm@delodi.net>
2013-11-20 10:53:39 +0100

    kiddifyKIDDIFY-111

--- public/Symfony/app/AppKernel.php
+++ public/Symfony/app/AppKernel.php
@@ -18,6 +18,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Kiddify\Bundle\WebsiteBundle\KiddifyWebsiteBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
